0a1f833d921d133a18a5b4d6205ca90977a13729
hector quintero <chavarro20@gmail.com>
2024-11-19 00:44:26 -0500

    Configuración inicial de Vue con dependencias y Vue Router

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,22 @@
-import './bootstrap';
+// Importa Bootstrap
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-import Alpine from 'alpinejs';
+// Importa FontAwesome
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faBank, faBuilding, faPencil, faPhone, faPlus, faTag, faTrash, faUser } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome"; // Asegúrate de importar correctamente
 
-window.Alpine = Alpine;
+// Importa Vue y el componente principal
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
 
-Alpine.start();
+// Agrega los iconos a la librería de FontAwesome
+library.add(faPhone, faUser, faTrash, faPencil, faTag, faBuilding, faBank, faPlus);
+
+// Crea la aplicación de Vue
+createApp(App)
+  .component("font-awesome-icon", FontAwesomeIcon) // Corregido el nombre del componente
+  .use(router)
+  .mount("#app");
